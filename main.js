415e3cef9aad3dfe485903b0fa430fbad8524bf5
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-02-25 23:04:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function setup(){
     video.hide();
     
 }
+
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detexting Objects";
@@ -30,35 +31,41 @@ function draw(){
         g = random(255);
         b = random(255);
         objectDetector.detect(video,gotResult);
-        for (i = 0; i < objects.lenght; i++){
-           document.getElementById("status").innerHTML = " Status: Objetos detectados";
+        
+        for (i = 0; i < objects.lenght; i++)
+        {
+            document.getElementById("status").innerHTML = " Status: Objetos detectados";
             fill(r,g,b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
             stroke(r,g,b);
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-        }
-if(objects[i].label == "person")
-{
-    document.getElementById("numberOfObjects").innerHTML = "Bebêencontrado";
-    console.log("stop");
-    song.stop();
-}
-        else
-        {
-            document.getElementById("numberOfObjects").innerHTML = "Bebê não  encontrado";
-            console.log("play");
-            song.play();
-        }
+         //tinha um fecha chaves aqui, mas estava no lugar errado
+        
+            if(objects[i].label == "person")
+            {
+                document.getElementById("numberOfObjects").innerHTML = "Bebê encontrado";
+                console.log("stop");
+                song.stop();
+            }
+            else
+            {
+                document.getElementById("numberOfObjects").innerHTML = "Bebê não  encontrado";
+                console.log("play");
+                song.play();
+            }
+        } //faltou esse fecha chaves no código (esse é para finalizar o for)
+        
         if(objects.lenght == 0)
         {
-document.getElementById("numberOfObjects").innerHTML = "BebÊ não encontrado"
+            document.getElementById("numberOfObjects").innerHTML = "Bebê não encontrado"
             console.log("play");
             song.play();
-    }
-    
+        }
+    } //faltou esse fecha chaves no código (esse é para finalizar o primeiro if
 }
+    
 function modelLoaded(){
     console.log("Model Loaded!")
     status = true;
@@ -67,7 +74,6 @@ function modelLoaded(){
 function gotResult(error, results){
     if (error) {
         console.log(error)
-        objects = results;
     }
     console.log(results);
     objects = results;
